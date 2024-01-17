e9635b24d2d477f35e3ad34cadd5f633fd316a32
Dell-User <Dell-User@DESKTOP-S7HKJ5C>
2024-01-17 10:07:05 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("This is Zanele",, "Greetings")
+    End Sub
+End Class
